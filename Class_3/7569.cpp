742fae8d58c495ad416d7325b8172eca14c4ba53
Suyoung-Seo <cprypt@gmail.com>
2024-08-02 18:13:13 +0900

    a

--- Class_3/7569.cpp
+++ Class_3/7569.cpp
@@ -13,10 +13,9 @@ using namespace std;
 
 int main()
 {
-    /*
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);*/
+    cout.tie(NULL);
 
     int M, N, H;
     cin >> M >> N >> H;
